740aec1ff9d6d83b99bb5553ad0cd64e06858164
Eddie Maceda <eddiemaceda@Eddies-MacBook-Air.local>
2023-10-26 20:16:08 -0400

    Edit main h1 text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,9 +5,9 @@ const Main = () => {
         <img src="https://images.unsplash.com/photo-1654009603731-20b6d7536002?auto=format&fit=crop&q=80&w=2864&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       </div>
       <div className="text-div">
-        <h1>Share what you know we'll help you get paid for it</h1>
+        <h1>Think ahead, and we'll help prepare you for the future</h1>
         <p>
-          Teachable helps creators engage their online audiences and get <br />
+          We help leaders engage their online audiences and get <br />
           paid on their own terms with courses, coaching, and downloadable content.
         </p>
         <p>Enter your email:</p>
