d45c1a749441847756f59ad136e52e960984cf10
Eddie Maceda <eddiemaceda@Eddies-MacBook-Air.local>
2023-10-27 18:08:47 -0400

    Create and style Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Hero />
     </>
   );
 }
