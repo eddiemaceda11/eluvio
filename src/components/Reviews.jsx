0fbaabc0b42fa00c0cb32667d0b57617a5e04149
Eddie Maceda <eddiemaceda11@gmail.com>
2023-11-04 11:16:25 -0400

    Add review comment

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -6,6 +6,9 @@ const Reviews = () => {
         <h4>Jane</h4>
         <h5>Director of Cyber Security @ Geneva Inc.</h5>
         <div className="mid-border"></div>
+        <p>
+          <i>By the far the most effective AI solution on the market. The techonology is cutting edge, and the support is unmatched.</i>
+        </p>
       </div>
     </section>
   );
