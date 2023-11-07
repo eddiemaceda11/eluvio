1ecdf1b0b34a59e2b21b7c85cf468e17c603fd8d
Eddie Maceda <eddiemaceda11@gmail.com>
2023-11-07 18:23:37 -0500

    Create dummy Company data

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,3 +1,22 @@
+const dummyCompanyData = [
+  {
+    name: "Company 1",
+    founded: 1981,
+  },
+  {
+    name: "Company 2",
+    founded: 2017,
+  },
+  {
+    name: "Company 3",
+    founded: 2020,
+  },
+  {
+    name: "Company 4",
+    founded: 1974,
+  },
+];
+
 const Companies = () => {
   return (
     <section className="companies">
