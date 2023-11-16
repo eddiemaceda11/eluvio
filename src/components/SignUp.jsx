0714fe3826ea0acc2b5adf4c94f10756371b889e
Eddie Maceda <eddiemaceda11@gmail.com>
2023-11-15 19:14:56 -0500

    Create steps array

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,3 +1,22 @@
+const steps = [
+  {
+    step: 1,
+    label: "Your Info",
+  },
+  {
+    step: 2,
+    label: "Select Plan",
+  },
+  {
+    step: 3,
+    label: "Add-Ons",
+  },
+  {
+    step: 4,
+    label: "Summary",
+  },
+];
+
 export default function SignUp() {
   return <div className="signup-section"></div>;
 }
