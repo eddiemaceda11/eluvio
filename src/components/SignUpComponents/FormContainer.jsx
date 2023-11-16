f52cf9464bdeaf2127bab1f225b9338c09262531
Eddie Maceda <eddiemaceda11@gmail.com>
2023-11-16 18:04:24 -0500

    Temp comment out of FormContainer

--- src/components/SignUpComponents/FormContainer.jsx
+++ src/components/SignUpComponents/FormContainer.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-export default function FormContainer({ steps }) {
-  const [currentStep, setCurrentStep] = useState(3);
+export default function FormContainer(/*{steps}*/) {
+  // const [currentStep, setCurrentStep] = useState(3);
 
-  const changeCurrentStep = () => {
-    setCurrentStep((prevState) => (prevState === 1 ? 2 : 1));
-  };
+  // const changeCurrentStep = () => {
+  //   setCurrentStep((prevState) => (prevState === 1 ? 2 : 1));
+  // };
 
   return (
     <section className="form-container">
