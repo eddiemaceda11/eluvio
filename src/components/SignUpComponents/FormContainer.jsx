9c9e3ae04438ce5ab4e24c1cfa61d6dd2012c50a
Eddie Maceda <eddiemaceda11@gmail.com>
2023-11-16 17:59:28 -0500

    Create FormContainer Component and import useState

---
+++ src/components/SignUpComponents/FormContainer.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function FormContainer({ steps }) {
+  const [currentStep, setCurrentStep] = useState(3);
+
+  const changeCurrentStep = () => {
+    setCurrentStep((prevState) => (prevState === 1 ? 2 : 1));
+  };
+
+  return (
+    <section className="form-container">
+      <StepsContainer
+        steps={steps}
+        currentStep={currentStep}
+        onClick={changeCurrentStep}
+      />
+      <InfoContainer currentStep={currentStep} />
+    </section>
+  );
+}
