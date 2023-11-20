cdbc12c023cc86977be3161b43c98d6e5399d2b3
Eddie Maceda <eddiemaceda11@gmail.com>
2023-11-19 19:11:14 -0500

    Refactor PersonalInfo

---
+++ src/components/SignUpComponents/PersonalInfo.jsx
@@ -0,0 +1,34 @@
+const PersonalInfo = () => {
+  return (
+    <div className="personal-info">
+      <h1>Personal Info</h1>
+      <p>Please provide your name, email, address, and phone number</p>
+      <form>
+        <div className="divvy">
+          <label>Name</label>
+          <input
+            type="text"
+            placeholder="e.g. Stephen King"
+          />
+        </div>
+        <div>
+          <label>Email Address</label>
+          <input
+            type="email"
+            placeholder="e.g. stephenking@lorem.com"
+          />
+        </div>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            placeholder="e.g. +1 234 567 8900"
+          />
+        </div>
+      </form>
+      <button className="plan-next-step-btn">Next Step</button>
+    </div>
+  );
+};
+
+export default PersonalInfo;
