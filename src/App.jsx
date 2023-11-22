3cef1797f309d30329580d6e5c6acb143e1350f0
Eddie Maceda <eddiemaceda11@gmail.com>
2023-11-22 02:19:00 -0500

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
 import "./App.css";
-
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Hero from "./components/Hero";
-import BusinessPlan from "./components/BusinessPlan";
-import Faqs from "./components/Faqs";
-import Reviews from "./components/Reviews";
-import Companies from "./components/Companies";
-import SignUp from "./components/SignUpComponents/SignUp";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Hero />
-      <BusinessPlan />
-      <SignUp />
-      <Faqs />
-      <Reviews />
-      <Companies />
-    </>
-  );
+  return <Homepage />;
 }
 
 export default App;
