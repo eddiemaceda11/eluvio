e8ae7d486d1d79aa686e1bb1d3d29ce2351b21e1
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-03 11:40:58 -0500

    Delete comment

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,4 +1,4 @@
-import Header from "../components/Header/Header"; //update import git
+import Header from "../components/Header/Header";
 import Main from "../components/Main";
 import Hero from "../components/Hero";
 import BusinessPlan from "../components/BusinessPlan";
