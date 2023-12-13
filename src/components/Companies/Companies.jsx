89884bbfba5543bd7a8149db300f2fc1d42653ec
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-12 19:56:29 -0500

    Import updated Companies component

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,42 @@
+import Company from "./Company";
+
+const dummyCompanyData = [
+  {
+    name: "Company 1",
+    founded: 1981,
+    id: 1,
+  },
+  {
+    name: "Company 2",
+    founded: 2017,
+    id: 2,
+  },
+  {
+    name: "Company 3",
+    founded: 2020,
+    id: 3,
+  },
+  {
+    name: "Company 4",
+    founded: 1974,
+    id: 4,
+  },
+  {
+    name: "Company 5",
+    founded: 1997,
+    id: 5,
+  },
+];
+
+const Companies = () => {
+  return (
+    <section className="companies">
+      <div className="logos">
+        <Company />
+        <Company />
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
