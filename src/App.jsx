c939b9eb39e026c4dc5b80fe25df5e4b92f77a61
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-12 19:54:52 -0500

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import "./App.css";
+import { createContext, useState, useEffect } from "react"; //imported git
+
 import Homepage from "./pages/Homepage";
 
+export const WindowContext = createContext();
+
 function App() {
-  return <Homepage />;
+  const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  console.log(windowSize[0]);
+
+  return (
+    <WindowContext.Provider value={windowSize}>
+      <Homepage />
+    </WindowContext.Provider>
+  );
 }
 
 export default App;
