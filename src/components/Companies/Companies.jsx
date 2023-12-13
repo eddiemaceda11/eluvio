1e749992a1d7424e342c79c356fd8561dbda9f00
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-13 18:32:51 -0500

    Delete dummy data

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -1,34 +1,6 @@
 import Company from "./Company";
 import "./Companies.css";
 
-const dummyCompanyData = [
-  {
-    name: "Company 1",
-    founded: 1981,
-    id: 1,
-  },
-  {
-    name: "Company 2",
-    founded: 2017,
-    id: 2,
-  },
-  {
-    name: "Company 3",
-    founded: 2020,
-    id: 3,
-  },
-  {
-    name: "Company 4",
-    founded: 1974,
-    id: 4,
-  },
-  {
-    name: "Company 5",
-    founded: 1997,
-    id: 5,
-  },
-];
-
 const Companies = () => {
   return (
     <section className="companies">
