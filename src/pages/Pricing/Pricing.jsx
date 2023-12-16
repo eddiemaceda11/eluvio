55cab01aea5ba38f5456bdfbad13f85a153017e8
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-16 17:55:06 -0500

    Update file imports in Pricing.jsx

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,11 +1,11 @@
-import Header from "../components/Header/Header";
+import Header from "../../components/Header/Header";
 import Main from "../components/Main";
-import Hero from "../components/Hero";
-import BusinessPlan from "../components/BusinessPlan";
-import Faqs from "../components/Faqs";
-import Reviews from "../components/Reviews";
+import Hero from "../../components/Hero";
+import BusinessPlan from "../../components/BusinessPlan";
+import Faqs from "../../components/Faqs";
+import Reviews from "../../components/Reviews";
 import Companies from "../components/Companies";
-import SignUp from "../components/SignUpComponents/SignUp";
+import SignUp from "../../components/SignUpComponents/SignUp";
 
 const Pricing = () => {
   return <div className="pricing-page"></div>;
