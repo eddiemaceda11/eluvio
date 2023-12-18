3458c4160bc151d0ba0f3640aafa51fd3d8dc5ca
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-18 18:49:06 -0500

    Create Sidebar component and import Sidebar.css file

---
+++ src/components/DashboardPage/Sidebar/Sidebar.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./Sidebar.css";
+
+// import { sidebarData } from "../../../data/dashboard/sidebar";
+
+export default function Dashboard() {
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+
+  function toggleSidebar() {
+    setSidebarIsOpen((prevState) => !prevState);
+  }
+
+  return (
+    <section className={sidebarIsOpen ? "sidebar" : "sidebar-closed"}>
+      <div className="sidebar-container">
+        {sidebarIsOpen && (
+          <>
+            <div className="dashboard-logo">
+              <p>e:luvio</p>
+            </div>
+            <div className="sidebar-selections">
+              {sidebarData.map((data) => (
+                <div
+                  className="sidebar-category"
+                  key={data.title}
+                >
+                  <p className="sidebar-title">{data.title}</p>
+                  <ul>
+                    {data.selections.map((selection) => (
+                      <div
+                        className="sidebar-selection-div"
+                        key={selection.selection}
+                      >
+                        <i className={selection.image}></i>
+                        <li className="sidebar-selection">{selection.selection}</li>
+                      </div>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+        {!sidebarIsOpen && <button onClick={toggleSidebar}>open</button>}
+      </div>
+    </section>
+  );
+}
