2f89be039b0d6754cb745e0b577977470c30b49b
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-17 20:12:01 -0500

    Create App folder

---
+++ src/App/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { createContext, useState, useEffect } from "react"; //imported git
+
+import Homepage from "../pages/Homepage/Homepage";
+import DashboardPage from "../pages/Dashboard/Dashboard";
+
+// context to check window width on resize, for use in Cards component
+export const WindowContext = createContext();
+
+function App() {
+  const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  console.log(windowSize[0]);
+
+  return (
+    <WindowContext.Provider value={windowSize}>
+      <Homepage />
+    </WindowContext.Provider>
+  );
+}
+
+export default App;
