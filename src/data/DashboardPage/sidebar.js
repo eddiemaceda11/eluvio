ba27cfdfcf71cf939925549ae38f3dc49877b9e6
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-17 20:06:09 -0500

    Create data folder with sidebar data file

---
+++ src/data/DashboardPage/sidebar.js
@@ -0,0 +1,77 @@
+const sidebarData = [
+  {
+    title: "Dashboard",
+    selections: [
+      {
+        selection: "Home",
+        image: "fa-solid fa-warehouse",
+      },
+    ],
+  },
+  {
+    title: "Pages",
+    selections: [
+      {
+        selection: "Orders",
+        image: "fa-solid fa-money-check-dollar",
+      },
+      {
+        selection: "Employees",
+        image: "fa-solid fa-user-tie",
+      },
+      {
+        selection: "Customers",
+        image: "fa-solid fa-bag-shopping",
+      },
+    ],
+  },
+  {
+    title: "Apps",
+    selections: [
+      {
+        selection: "Calender",
+        image: "fa-solid fa-calendar-days",
+      },
+      {
+        selection: "Themes",
+        image: "fa-solid fa-palette",
+      },
+      {
+        selection: "Editor",
+        image: "fa-solid fa-pen-to-square",
+      },
+    ],
+  },
+  {
+    title: "Charts",
+    selections: [
+      {
+        selection: "Line Chart",
+        image: "fa-solid fa-chart-line",
+      },
+      {
+        selection: "Area Chart",
+        image: "fa-solid fa-chart-area",
+      },
+      {
+        selection: "Bar Chart",
+        image: "fa-solid fa-chart-simple",
+      },
+    ],
+  },
+  {
+    title: "Support",
+    selections: [
+      {
+        selection: "Live Chat",
+        image: "fa-solid fa-headset",
+      },
+      {
+        selection: "Email",
+        image: "fa-solid fa-envelope",
+      },
+    ],
+  },
+];
+
+export { sidebarData };
