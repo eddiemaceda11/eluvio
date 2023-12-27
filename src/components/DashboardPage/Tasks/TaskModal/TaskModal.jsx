3596fb4f0da3e1479da9d2f0d685b3e8c01ae1ca
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-27 16:43:13 -0500

    Create TaskModal component

---
+++ src/components/DashboardPage/Tasks/TaskModal/TaskModal.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./TaskModal.css";
+
+const allTasks = {};
+
+const newTask = {
+  taskName: "",
+  admin: "",
+  priority: "",
+  status: "",
+  due: "",
+};
+
+export default function TaskModal() {
+  const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState("");
+  const [admin, setAdmin] = useState("");
+  const [priority, setPriority] = useState("");
+  const [status, setStatus] = useState("");
+  const [due, setDue] = useState("");
+
+  console.log(tasks);
+
+  const addTask = () => {
+    setTasks([
+      ...tasks,
+      {
+        taskName,
+        admin,
+        priority,
+        status,
+        due,
+      },
+    ]);
+  };
+
+  return (
+    <div className="task-modal">
+      <div className="task-modal-container">
+        <div>
+          <p>Task Name</p>
+          <input
+            required
+            type="text"
+            onChange={(e) => setTaskName(e.target.value)}
+          />
+        </div>
+        <div>
+          <p>Admin</p>
+          <input
+            required
+            type="text"
+            onChange={(e) => setAdmin(e.target.value)}
+          />
+        </div>
+        <div>
+          <p>Priority</p>
+          <input
+            required
+            type="text"
+            onChange={(e) => setPriority(e.target.value)}
+          />
+        </div>
+        <div>
+          <p>Status</p>
+          <input
+            required
+            type="text"
+            onChange={(e) => setStatus(e.target.value)}
+          />
+        </div>
+        <div>
+          <p>Due by</p>
+          <input
+            required
+            type="text"
+            onChange={(e) => setDue(e.target.value)}
+          />
+        </div>
+        <button onClick={addTask}>submit</button>
+      </div>
+      {tasks.length > 0 && <p>{tasks[0].taskName}</p>}{" "}
+    </div>
+  );
+}
