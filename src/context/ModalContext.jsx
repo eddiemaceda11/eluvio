b14eda59e02528eb22cc17904f4d0fb6d7f747bb
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-29 15:22:52 -0500

    Create ModalContext

---
+++ src/context/ModalContext.jsx
@@ -0,0 +1,14 @@
+import { useState, createContext } from "react";
+
+const ModalContext = createContext(null);
+
+const ModalContextProvider = ({ children }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleModalToggle = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  return <ModalContext.Provider value={{ modalOpen, handleModalToggle }}>{children}</ModalContext.Provider>;
+};
+
+export { ModalContext, ModalContextProvider };
