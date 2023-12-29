03a3d24969f6d6687e2b70920de7f0f4edf0494c
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-29 15:31:49 -0500

    Update Sidebar data import

--- src/components/DashboardPage/Sidebar/Sidebar.jsx
+++ src/components/DashboardPage/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Sidebar.css";
 
-import { sidebarData } from "../../../data/dashboard/sidebar";
+import { sidebarData } from "../../../data/DashboardPage/sidebar";
 
 export default function Dashboard() {
   const [sidebarIsOpen, setSidebarIsOpen] = useState(true);
