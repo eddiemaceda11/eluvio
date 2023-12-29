5aa6ebd2de0c183fac01586164541fc6e491d877
Eddie Maceda <eddiemaceda11@gmail.com>
2023-12-29 15:20:13 -0500

    Create DashboardPage component

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -7,7 +7,26 @@ import Apps from "../../components/DashboardPage/Apps/Apps";
 import TaskModal from "../../components/DashboardPage/Tasks/TaskModal/TaskModal";
 
 const DashboardPage = () => {
-  return <div></div>;
+  const { modalOpen } = useContext(ModalContext);
+
+  return (
+    <>
+      <div className="dashboard">
+        <div className="container">
+          <Sidebar />
+          <div className="dashboard-right-side">
+            <DashboardHeader />
+            <Tasks />
+            <div className="features">
+              <Charts />
+              <Apps />
+            </div>
+          </div>
+        </div>
+      </div>
+      {modalOpen && <TaskModal />}
+    </>
+  );
 };
 
 export default DashboardPage;
