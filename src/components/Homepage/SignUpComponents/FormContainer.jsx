5848e74946196bc63b00f1346f215304e485810e
Eddie Maceda <eddiemaceda11@gmail.com>
2024-01-03 21:45:49 -0500

    Comment out changeCurrentStep function

--- src/components/Homepage/SignUpComponents/FormContainer.jsx
+++ src/components/Homepage/SignUpComponents/FormContainer.jsx
@@ -5,22 +5,22 @@ import InfoContainer from "./InfoContainer";
 export default function FormContainer({ steps }) {
   const [currentStep, setCurrentStep] = useState(1);
 
-  const changeCurrentStep = () => {
-    setCurrentStep((prevState) => {
-      if (prevState === 1) return 2;
-      if (prevState === 2) return 3;
-      if (prevState === 3) return 4;
-      if (prevState === 4) return 1;
-    });
-  };
+  // const changeCurrentStep = () => {
+  //   setCurrentStep((prevState) => {
+  //     if (prevState === 1) return 2;
+  //     if (prevState === 2) return 3;
+  //     if (prevState === 3) return 4;
+  //     if (prevState === 4) return 1;
+  //   });
+  // };
 
   return (
     <section className="signup-section">
       <div className="signup-form-container">
         <StepsContainer
           steps={steps}
-          currentStep={currentStep}
-          onClick={changeCurrentStep}
+          // currentStep={currentStep}
+          // onClick={changeCurrentStep}
         />
         <InfoContainer currentStep={currentStep} />
       </div>
