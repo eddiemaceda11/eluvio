28145a1f3e5ceccdfaa871d98406e75541223d0a
Eddie Maceda <eddiemaceda11@gmail.com>
2024-01-17 18:09:25 -0500

    Update header dropdown

--- src/components/Homepage/Header/Header.jsx
+++ src/components/Homepage/Header/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import HeaderDropdown from "./HeaderDropdown";
 import "./Header.css";
+import { useState } from "react";
 
 const Header = () => {
+  const [dropdownActive, setDropdownActive] = useState(false);
+
   return (
     <header>
       <div className="container">
         <h1>e:luvio</h1>
         <ul className="header-nav">
-          <li>
+          <li onClick={() => setDropdownActive(!dropdownActive)}>
             Product{" "}
             <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTQgNkw4IDEwTDEyIDYiIHN0cm9rZT0iIzIyMjIyMiIgc3Ryb2tlLXdpZHRoPSIxLjMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg==" />
           </li>
@@ -28,7 +31,7 @@ const Header = () => {
           </Link>
           <button>Start for free</button>
         </div>
-        <HeaderDropdown />
+        {dropdownActive ? <HeaderDropdown /> : ""}
       </div>
     </header>
   );
