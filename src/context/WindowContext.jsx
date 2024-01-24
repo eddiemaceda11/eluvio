98ea1684ae6199d24e2ac129033add309c55f68a
Eddie Maceda <eddiemaceda11@gmail.com>
2024-01-23 21:04:23 -0500

    Add descriptive comments to WindowContext

--- src/context/WindowContext.jsx
+++ src/context/WindowContext.jsx
@@ -3,14 +3,14 @@ import { useState, createContext, useEffect } from "react";
 const WindowContext = createContext();
 
 const WindowContextProvider = ({ children }) => {
+  // get the current height and width of window page
   const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
 
   useEffect(() => {
+    // update window height and width on resize
     const handleWindowResize = () => {
-      console.log(windowSize[0]);
       setWindowSize([window.innerWidth, window.innerHeight]);
     };
-
     window.addEventListener("resize", handleWindowResize);
 
     return () => {
