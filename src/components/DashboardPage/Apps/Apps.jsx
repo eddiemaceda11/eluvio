b8bc68ef025c38284ec313f3b2c12848469a4547
Eddie Maceda <eddiemaceda11@gmail.com>
2024-01-27 18:02:54 -0500

    Add Area chart component

--- src/components/DashboardPage/Apps/Apps.jsx
+++ src/components/DashboardPage/Apps/Apps.jsx
@@ -1,5 +1,97 @@
 import "./Apps.css";
 
+import { LineChart, Line, Legend, AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+
+const areaData = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
 export default function Apps() {
-  return <section className="db-apps"></section>;
+  return (
+    <section className="db-charts">
+      <ResponsiveContainer
+        width="95%"
+        height="90%"
+      >
+        <AreaChart
+          width={500}
+          height={400}
+          data={areaData}
+          margin={{
+            top: 40,
+            right: 30,
+            left: 20,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="uv"
+            stackId="1"
+            stroke="#e63946"
+            fill="#e63946"
+          />
+          <Area
+            type="monotone"
+            dataKey="pv"
+            stackId="1"
+            stroke="#fca311"
+            fill="#fca311"
+          />
+          <Area
+            type="monotone"
+            dataKey="amt"
+            stackId="1"
+            stroke="#90e0ef"
+            fill="#90e0ef"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </section>
+  );
 }
