7fb3190ef823d5614454d92877674b0edd1a527e
Eddie Maceda <eddiemaceda11@gmail.com>
2024-02-19 19:40:43 -0500

    Update Header text

--- src/components/Homepage/Header/Header.jsx
+++ src/components/Homepage/Header/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
             <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTQgNkw4IDEwTDEyIDYiIHN0cm9rZT0iIzIyMjIyMiIgc3Ryb2tlLXdpZHRoPSIxLjMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg==" />
           </li>
           <li>
-            <p>Investments</p>
+            <p>Financials</p>
           </li>
         </ul>
         <div className="header-rs">
