f5d204b4c0e9ea5e3286d07807739f3e3ea59006
Eddie Maceda <eddiemaceda11@gmail.com>
2024-02-22 18:16:35 -0500

    Comment out Header financial nav text

--- src/components/Homepage/Header/Header.tsx
+++ src/components/Homepage/Header/Header.tsx
@@ -25,9 +25,9 @@ const Header = () => {
             <p>Resources</p>
             <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTQgNkw4IDEwTDEyIDYiIHN0cm9rZT0iIzIyMjIyMiIgc3Ryb2tlLXdpZHRoPSIxLjMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg==" />
           </li>
-          <li>
+          {/* <li>
             <p>Financials</p>
-          </li>
+          </li> */}
         </ul>
         <div className="header-rs">
           <Link to="/dashboard">
