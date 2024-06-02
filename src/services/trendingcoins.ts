3f5cef135922efdad5d9cf1bc6067f6e6a76434f
Eddie Maceda <eddiemaceda11@gmail.com>
2024-06-02 15:24:41 -0400

    Update the trending coin type

--- src/services/trendingcoins.ts
+++ src/services/trendingcoins.ts
@@ -1,17 +1,24 @@
+import { Dispatch, SetStateAction } from "react";
+
 export type TrendingCoin = {
-  symbol: string;
-  price: string;
-  percentage: number;
-  coinImg: string;
-  // coinId: string;
+  item: {
+    symbol: string;
+    data: {
+      price: string;
+      price_change_percentage_24h: {
+        usd: string;
+      };
+    };
+    large: string;
+  };
 };
 
 // Function to fetch top 15 trending coins
-export const fetchTrendingCoins = async (setTrendingCoins) => {
+export const fetchTrendingCoins = async (setTrendingCoins: Dispatch<SetStateAction<TrendingCoin[]>>) => {
   const response = await fetch("https://api.coingecko.com/api/v3/search/trending");
   const data = await response.json();
   // destructure coins to only get necessary items
-  let structuredCoins = data.coins.map((coin) => {
+  let structuredCoins = data.coins.map((coin: TrendingCoin) => {
     return {
       coinSymbol: coin.item.symbol,
       coinPrice: coin.item.data.price,
